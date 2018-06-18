e4dd7e54ea704d65b67e6c963d026e6cf6a00523
github username <github email address>
2018-06-18 14:38:20 +0100

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,20 @@
+require 'pry'
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
   def song_count
     # return the number of songs in a genre
+    self.songs.all.length
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.songs.uniq {|el| el.artist}.length
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    songs.map {|el| el.artist.name}.uniq
   end
 end
